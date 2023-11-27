5148a8928245d80e395565aaa564f2517fba7c82
Sean McGrady <sean.padraic.clayton.mcgrady@gmail.com>
2023-11-27 20:21:30 -0300

    Update: AbstractBaseUser implementation

--- src/server/server/models.py
+++ src/server/server/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Group, Permission
 
 
 class Diagnostic(models.Model):
@@ -17,11 +18,26 @@ class Region(models.Model):
         app_label = 'server'
     
 
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    email = models.EmailField()
-    password = models.CharField(max_length=100)
-    bookmarks = models.ManyToManyField(Diagnostic)
+# class User(models.Model):
+#     username = models.CharField(max_length=100)
+#     email = models.EmailField(unique=True)
+#     password = models.CharField(max_length=100)
+#     bookmarks = models.ManyToManyField(Diagnostic)
     
-    class Meta:
-        app_label = 'server'
+#     class Meta:
+#         app_label = 'server'
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100, default="")
+    last_name = models.CharField(max_length=100, default="")
+    bookmarks = models.ManyToManyField(Diagnostic)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    groups = models.ManyToManyField(Group, blank=True, related_name='user_custom_groups') 
+    user_permissions = models.ManyToManyField(Permission, blank=True, related_name='user_custom_permissions')
+    
+    
+    
