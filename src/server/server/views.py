12f4eac362f12ec038396a6495cb408bad9e3c17
Sean McGrady <sean.padraic.clayton.mcgrady@gmail.com>
2023-11-28 18:27:25 -0300

    Feat: Auth (WIP)

--- src/server/server/views.py
+++ src/server/server/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from .models import Diagnostic, Region, User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.hashers import make_password
 
 def read_all_diagnostics(request):
     all_records = Diagnostic.objects.all()
@@ -15,3 +17,23 @@ def read_all_regions(request):
     data = [{'name': record.name} for record in all_records]
     
     return JsonResponse({'data': data})
+
+def sign_in(request):
+    
+    if request.method == 'POST':
+        data = request.POST
+        
+        username = data.get('username')
+        password = data.get('password')
+        
+        user = authenticate(username=username, password=password)
+        
+        print(user, "user heeeeeeeeeeeeeeeeeeeeeeeeeeeeere")
+    
+        if user is not None:
+            login(request, user)
+            return HttpResponse("Sign-in successful")
+        else: 
+            return HttpResponse("Sign-in failed.")
+    
+    
