38cd9a39893434d4f9c7f97c1d2e44bec5cdacda
Sean McGrady <sean.padraic.clayton.mcgrady@gmail.com>
2023-11-29 17:08:43 -0300

    Cleaning: Comment removal

--- src/server/server/models.py
+++ src/server/server/models.py
@@ -17,23 +17,6 @@ class Region(models.Model):
     class Meta:
         app_label = 'server'
     
-
-# class User(AbstractBaseUser, PermissionsMixin):
-#     email = models.EmailField(unique=True)
-#     first_name = models.CharField(max_length=100, default="")
-#     last_name = models.CharField(max_length=100, default="")
-#     bookmarks = models.ManyToManyField(Diagnostic)
-#     is_active = models.BooleanField(default=True)
-#     is_staff = models.BooleanField(default=False)
-    
-#     USERNAME_FIELD = 'email'
-    
-#     groups = models.ManyToManyField(Group, blank=True, related_name='user_custom_groups') 
-#     user_permissions = models.ManyToManyField(Permission, blank=True, related_name='user_custom_permissions')
-    
-#     class Meta:
-#         app_label = 'server'
-    
     
 class User(AbstractUser):
     bookmarks = models.ManyToManyField(Diagnostic)
